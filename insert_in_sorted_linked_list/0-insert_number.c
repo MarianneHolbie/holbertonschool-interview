e0868a848b6eb1bd3030de3f118c77fac885dd5a
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-12 10:52:17 +0000

    test code

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,36 +2,38 @@
 #include "lists.h"
 
 /**
- * insert_node - insert a new node into a sorted linked list
- * @head: pointer to pointer of first node of listint_t list
- * @number: integer to be insert in new node
- * Return: address of the new element or NULL if it fails
- */
+* insert_node - insert a new node
+* @head: head of the linked list
+* @number: number to insert in the linked list
+* return: address of the new node or NULL if it failed
+*/
+
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
-    
-    current = *head;
+  listint_t *new;
+  listint_t *current;
 
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-    return (NULL);
+  current = *head;
 
-new->n = number;
+  new = malloc(sizeof(listint_t));
+  if (new == NULL)
+    return(NULL);
 
-while (current && current->next && number > current->next->n)
+  new->n = number;
+
+  while(current && current->next && current->next->n < number)
     current = current->next;
 
-if ((current == NULL) || (current->n > number))
-{
+  if((current == NULL) || (number < current->n))
+  {
     new->next = *head;
     *head = new;
-}
-else
-{
+  }
+  else
+  {
     new->next = current->next;
     current->next = new;
-}
-return (new);
+  }
+
+  return(new);
 }
