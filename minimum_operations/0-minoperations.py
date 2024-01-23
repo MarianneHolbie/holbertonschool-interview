91e01bd5d6ba308b2d5e3e6e4cce39b7b48fd929
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-23 10:59:10 +0100

    Add readme + task 0 min operation

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Min Operations
+"""
+
+
+def minOperations(n):
+    """
+        function to define the fewest number of operations needed
+        to result in exactly n H characters in the file.
+
+        :param n: number
+
+        :return: int or 0 if n is impossible to achieve
+    """
+    if n <= 1:
+        return 0
+
+    min_op = [float('inf')] * (n + 1)
+
+    # initialize min_op, if one H is already in
+    min_op[1] = 0
+
+    # loop 2 to n
+    for i in range(2, n + 1):
+        for j in range(1, i // 2 + 1):
+            if i % j == 0:
+                min_op[i] = min(min_op[i], min_op[j] + i // j)
+
+    return min_op[n]
