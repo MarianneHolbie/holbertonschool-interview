b763a1b32d82db90a8cea42765ccc79d2aad7cc5
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-02-15 11:00:28 +0100

    add \n in print

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -43,7 +43,7 @@ try:
                 # print in sorted order
                 for code, count_code in sorted(count_status_code.items()):
                     if count_code != 0:
-                        print('{}: {}\n'.format(code, count_status_code[code]))
+                        print('{}: {}'.format(code, count_status_code[code]))
                 # reinitialized count line
                 count_line = 0
 
