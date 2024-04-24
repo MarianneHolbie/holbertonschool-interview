014726503caaa9ddf4c0beb1a6f0424ab107a5f0
Arrue Marianne <5608@holbertonstudents.com>
2024-04-24 01:09:17 -0700

    fix semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -6,8 +6,8 @@ const request = require('request');
 // use process to store argv in command line
 const args = process.argv;
 if (args.length !== 3 || isNaN(parseInt(args[2]))) {
-    console.error('Usage: ./script.js <movie_id>');
-    process.exit(1);
+  console.error('Usage: ./script.js <movie_id>');
+  process.exit(1);
 }
 // construct request url by adding id film
 const movieId = parseInt(args[2]);
@@ -15,34 +15,34 @@ const URL = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 // print all characters of one episode in rigth order
 request.get(URL, (err, response, body) => {
-    if (err) {
-      console.error('error', err);
-      process.exit(1);
-    } 
-    if (response.statusCode !== 200) {
-        console.error(`HTTP Error: ${response.statusCode} - ${response.statusMessage}`);
-        process.exit(1);
-    }
-    
-    const characters = JSON.parse(body).characters;
-    const promises = characters.map(characterUrl => {
-        return new Promise((resolve, reject) => {
-            request.get(characterUrl, (err, response, body) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(JSON.parse(body).name);
-                }
-            });
-        });
-    });
+  if (err) {
+    console.error('error', err);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error(`HTTP Error: ${response.statusCode} - ${response.statusMessage}`);
+    process.exit(1);
+  }
 
-    Promise.all(promises)
-        .then(names => {
-            names.forEach(name => console.log(name));
-        })
-        .catch(err => {
-            console.error('Error:', err);
-            process.exit(1);
-        });
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request.get(characterUrl, (err, response, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(err => {
+      console.error('Error:', err);
+      process.exit(1);
+    });
 });
