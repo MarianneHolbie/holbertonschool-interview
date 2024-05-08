64734036389816cee4c6c63145bca7a92c4eb890
Arrue Marianne <5608@holbertonstudents.com>
2024-05-08 01:17:19 -0700

    solve betty style

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -6,7 +6,7 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void swap(int* a, int* b);
+void swap(int *a, int *b);
 void heapify(int arr[], size_t size, size_t calcsize, int i);
 
 
