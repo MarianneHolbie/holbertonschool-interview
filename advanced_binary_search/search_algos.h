ff0c2a64c067224a43308083e15cbdc857b3911e
Arrue Marianne <5608@holbertonstudents.com>
2024-05-14 00:19:11 -0700

    Advanced binary search task

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_H__
+#define _SEARCH_ALGOS_H__
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H__ */
