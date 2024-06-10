913492af3cce23c2cb7f13380557ec04c6db95c6
Arrue Marianne <5608@holbertonstudents.com>
2024-06-10 12:42:09 -0700

    solve pycodestyle

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-    Function to rotate a 2D matrix 90 degrees clockwise    
+Function to rotate a 2D matrix 90 degrees clockwise
 """
-
 
 
 def rotate_2d_matrix(matrix):
@@ -10,9 +9,7 @@ def rotate_2d_matrix(matrix):
         Function to rotate a 2D matrix 90 degrees clockwise
         Args:
             matrix: 2D matrix to rotate
-        
         Returns: transposed matrix
-
     """
     # store the length of the matrix
     n = len(matrix)
@@ -23,13 +20,17 @@ def rotate_2d_matrix(matrix):
         for j in range(i, n - i - 1):
             # store the current element
             temp = matrix[i][j]
-            # replace the current element with the element at the bottom left
+            # replace the current element with the element at
+            # the bottom left
             matrix[i][j] = matrix[n - 1 - j][i]
-            # replace the element at the bottom left with the element at the bottom right
+            # replace the element at the bottom left with the element
+            # at the bottom right
             matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-            # replace the element at the bottom right with the element at the top right
+            # replace the element at the bottom right with the element
+            # at the top right
             matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-            # replace the element at the top right with the current element
+            # replace the element at the top right with the current
+            # element
             matrix[j][n - 1 - i] = temp
 
     return matrix
