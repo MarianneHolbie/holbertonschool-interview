80045590740f6d4b1a388b5be940f6c979ab2780
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-30 15:42:42 +0200

    function to calculate perimeter of island in a grid

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+        function to compute perimeter of the island described in grid
+
+    :param grid: list of int,
+            0: water
+            1: land
+            each cell is square with side lenght of 1
+
+    :return: perimeter of the corresponding island
+    """
+    perimeter = 0
+    nbr_row = len(grid)
+    nbr_col = len(grid[0])
+
+    # add zero border
+    grid = [x + [0] for x in grid] + [[0] * nbr_col]
+
+    # read grid
+    for row in range(nbr_row):
+        for col in range(nbr_col):
+            if grid[row][col] == 1:
+                perimeter += 4 - 2 *(grid[row + 1][col] + grid[row][col + 1])
+
+    return perimeter
