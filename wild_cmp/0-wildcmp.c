1fcba029fac4e200be989836b46268d7a4b2b80a
Arrue Marianne <5608@holbertonstudents.com>
2024-08-12 05:36:12 -0700

    without loop

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -4,24 +4,26 @@
 int wildcmp(char *s1, char *s2)
 {
     /* id if same end */
-    if (*s2 == '\0')
-        return (*s1 == '\0');
-    
-    /* If s2 is not '*' */
-     if (*s2 != '*')
-    {
-        if (*s1 == '\0' || (*s1 != *s2 && *s2 != '?'))
-            return 0;
-        return wildcmp(s1 + 1, s2 + 1);
-    }
+    if (*s1 == '\0' && *s2 == '\0')
+        return 1;
 
-    /* if '*' => next */
-    while (*s1 != '\0')
+    /* if char s2 is '*' */
+    if (*s2 == '*')
     {
-        if (wildcmp(s1, s2 + 1))
+        // if '*' last char of s2
+        if (*(s2 + 1) == '\0')
             return 1;
-        s1++;
-    }
 
-    return wildcmp(s1, s2 + 1);
+        // recursiv call or next char
+        if (*s1 == '\0')
+            return wildcmp(s1, s2 + 1);
+        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+    }   
+    
+    /* If same char for s1 and s2 */
+    if (*s1 == *s2)
+        return wildcmp(s1 + 1, s2 + 1);
+
+    /* different string*/
+    return 0;
 }
